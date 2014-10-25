43e0d7995c79ed12d3cbe7485a660fa248d03b22
Anton Lenev <m0rg0t.Anton@gmail.com>
2014-10-25 15:43:58 +0400

    init

---
+++ TransparentBudget/app.js
@@ -0,0 +1,87 @@
+﻿
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://transparentbudget2:poldyXAXA2@ds027748.mongolab.com:27748/Transparentbudget');
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    log.error('connection error:', err.message);
+});
+db.once('open', function callback() {
+    log.info("Connected to DB!");
+});
+
+var Schema = mongoose.Schema;
+
+// Schemas
+var User = new Schema({
+    /*kind: {
+        type: String,
+        enum: ['thumbnail', 'detail'],
+        required: true
+    },
+    url: { type: String, required: true }*/
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true }
+});
+
+var PlaceObject = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    description: { type: String, required: true },
+    modified: { type: Date, default: Date.now }
+});
+
+// validation
+PlaceObject.path('title').validate(function (v) {
+    return v.length > 5 && v.length < 70;
+});
+
+var PlaceObjectModel = mongoose.model('PlaceObject', PlaceObject);
+
+module.exports.PlaceObjectModel = PlaceObjectModel;
+
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/contact', routes.contact);
+app.get('/places', routes.places);
+app.get('/documents', routes.documents);
+app.get('/places_map', routes.places_map);
+
+app.get('places/add', routes.places_add);
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ TransparentBudget/routes/index.js
@@ -0,0 +1,33 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Прозрачный бюджет', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'О проекте', year: new Date().getFullYear(), message: 'Your application description page.' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Контакты', year: new Date().getFullYear(), message: 'Your contact page.' });
+};
+
+exports.places = function (req, res) {
+    res.render('places', { title: 'Места', year: new Date().getFullYear(), message: 'Your places page.' });
+};
+
+exports.documents = function (req, res) {
+    res.render('documents', { title: 'Документы', year: new Date().getFullYear(), message: 'Your documents page.' });
+};
+
+exports.places_map = function (req, res) {
+    res.render('places_map', { title: 'Карта мест', year: new Date().getFullYear(), message: 'Карта мест' });
+};
+
+exports.places_add = function(req, res) {
+    res.render('places_add', {});
+}
+
